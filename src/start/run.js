40c5bddc2927a589aaca8cdd5c9798868a4ed8ec
ulugbek erkinov <103308510+ulugbekerkinovich2@users.noreply.github.com>
2024-07-10 13:47:23 +0500

    Update run.js

--- src/start/run.js
+++ src/start/run.js
@@ -1,15 +1,24 @@
-const config = require ("../../config/index.js");
-const {connect} = require("mongoose");
+const config = require("../../config/index.js");
+const { connect } = require("mongoose");
 
 const run = async (app) => {
-    const mongoUri = config.mongoUri;
+    try {
+        const mongoUri = config.mongoUri;
 
-    await connect(mongoUri);
-    
-    const port = config.port;
-    app.listen(port, () => {
-        console.log(`Server running on port:${port}`);
-    });
+        await connect(mongoUri, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
+        console.log('MongoDB connected successfully');
+
+        const port = config.port;
+        app.listen(port, () => {
+            console.log(`Server running on port: ${port}`);
+        });
+    } catch (err) {
+        console.error('Failed to connect to MongoDB', err);
+        process.exit(1); // Exit the process with a failure code
+    }
 };
 
 module.exports = run;
