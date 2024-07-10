e6e24957ced6908f40ee2326522a26b2fa721c8a
ulugbek erkinov <103308510+ulugbekerkinovich2@users.noreply.github.com>
2024-07-10 13:48:29 +0500

    Update index.js

--- config/index.js
+++ config/index.js
@@ -5,7 +5,27 @@ const config = {
   jwt_secret_key: process.env.JWT_SECRET_KEY,
   jwt_secret_key_admin: process.env.JWT_SECRET_KEY_ADMIN,
   jwt_expires_in: process.env.JWT_EXPIRES_IN || '7d',
-  mongoUri: process.env.mongoUri
+  // mongoUri: process.env.mongoUri
 };
 
+
+
+
+require('dotenv').config();
+
+const dbUser = process.env.DB_USER;
+const dbPassword = encodeURIComponent(process.env.DB_PASSWORD); // Encode the password
+const dbUrl = process.env.DB_URL;
+const dbPort = process.env.DB_PORT;
+const dbName = process.env.DB_NAME;
+
+const mongoUri = `mongodb://${dbUser}:${dbPassword}@${dbUrl}:${dbPort}/${dbName}?authSource=admin`;
+
+module.exports = {
+    jwt_secret_key: process.env.JWT_SECRET_KEY,
+    jwt_secret_key_admin: process.env.JWT_SECRET_KEY_ADMIN,
+    jwt_expires_in: process.env.JWT_EXPIRES_IN || '7d',
+    mongoUri,
+    port: process.env.PORT || 3000
+};
 module.exports = config;
